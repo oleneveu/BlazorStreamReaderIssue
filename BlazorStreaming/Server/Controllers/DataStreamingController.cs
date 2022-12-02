8bc2c5a590961c9b20c255448f461ef8a794a596
oleneveu <oleneveu@jeanlutzsa.com>
2022-12-02 17:49:23 +0100

    Basic project that demonstrate an issue with the StreamReader when reading a response stream from a endpoint.

---
+++ BlazorStreaming/Server/Controllers/DataStreamingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorStreaming.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataStreamingController : ControllerBase
+    {
+        [HttpGet]
+        public async Task Get(CancellationToken cancellation)
+        {
+            Response.ContentType = "text/plain";
+            StreamWriter sw;
+            await using ((sw = new StreamWriter(Response.Body)).ConfigureAwait(false))
+            {
+                int counter = 0;
+                while (true)
+                {
+                    await sw.WriteLineAsync(counter.ToString("00000"));
+                    await sw.FlushAsync();
+                    //if (counter >= 10) break;
+                    await Task.Delay(1000, cancellation);
+                    counter++;
+                }
+            }
+        }
+    }
+}
