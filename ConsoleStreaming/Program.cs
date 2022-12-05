b8f78c3ca0ab638eade3324a015083c451c57226
oleneveu <oleneveu@jeanlutzsa.com>
2022-12-05 14:28:04 +0100

    Project 'ConsoleStreaming' added to the solution that highlights the different behavior of the StreamReader.ReadLineAsync() between Blazor and non-blazor environment.

---
+++ ConsoleStreaming/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleStreaming
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await BeginReadText();
+        }
+
+        static async Task BeginReadText()
+        {
+            var http = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:5125"),
+            };
+
+            using var request = new HttpRequestMessage(HttpMethod.Get, "DataStreaming");
+            using var response = await http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            var responseStream = await response.Content.ReadAsStreamAsync();
+
+            using (var sr = new StreamReader(responseStream, bufferSize: 128)) // 128: Minimum value for StreamReader
+            {
+                string? line;
+                while ((line = await sr.ReadLineAsync()) != null) // Not passing the cancellation token to be .Net 6 compatible. Passing the token has no effect on the issue.
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
